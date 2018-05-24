4c3ecad5827f1286ed1e99ba3ce72aa4ed79b679
Mateusz Bednarski <echelon133@gmail.com>
2018-05-24 14:01:16 +0200

    Added /rooms/{roomName} mapping to RoomController

--- src/main/java/ml/echelon133/controller/RoomController.java
+++ src/main/java/ml/echelon133/controller/RoomController.java
@@ -4,6 +4,8 @@ import ml.echelon133.model.Room;
 import ml.echelon133.service.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +25,11 @@ public class RoomController {
     @RequestMapping(value = "/json/rooms", method = RequestMethod.GET)
     public @ResponseBody List<Room> getAllRooms() {
         return roomService.getAllRooms();
+    }
+
+    @RequestMapping(value = "/rooms/{roomName}", method = RequestMethod.GET)
+    public String getRoom(@PathVariable String roomName, Model model) {
+        model.addAttribute("roomName", roomName);
+        return "room";
     }
 }
