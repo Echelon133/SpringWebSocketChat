4fde59360e144c9225aa947498ef5868ac175b7d
Mateusz Bednarski <echelon133@gmail.com>
2018-05-24 08:54:31 +0200

    Added SpringWebSocketChatApp main class

---
+++ src/main/java/ml/echelon133/SpringWebSocketChatApp.java
@@ -0,0 +1,12 @@
+package ml.echelon133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketChatApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketChatApp.class, args);
+    }
+}
