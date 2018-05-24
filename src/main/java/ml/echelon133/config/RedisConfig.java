2e4391d90047d41c3ad9fab77d81eead30baef21
Mateusz Bednarski <echelon133@gmail.com>
2018-05-24 10:56:59 +0200

    Added RedisConfig class

---
+++ src/main/java/ml/echelon133/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package ml.echelon133.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+@Configuration
+@PropertySource("classpath:application.yml")
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private Integer port;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(RedisPassword.of(redisPassword));
+        return new JedisConnectionFactory(config);
+    }
+}
