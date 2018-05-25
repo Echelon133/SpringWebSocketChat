0aa2a284684c3dd3b4927bfeca1d9159c1b6e15f
Mateusz Bednarski <echelon133@gmail.com>
2018-05-25 13:18:42 +0200

    Updated AdminController with /admin/rooms/{id} DELETE mapping

--- src/main/java/ml/echelon133/controller/AdminController.java
+++ src/main/java/ml/echelon133/controller/AdminController.java
@@ -10,10 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class AdminController {
@@ -47,6 +51,14 @@ public class AdminController {
         Room room = new Room(roomForm.getRoomName());
         roomService.save(room);
         return "redirect:/admin";
+    }
+
+    @RequestMapping(value = "/admin/rooms/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody Map<String, Boolean> deleteRoomWithId(@PathVariable String id) {
+        Map<String, Boolean> response = new HashMap<>();
+        Boolean wasDeleted = roomService.deleteById(id);
+        response.put("deleted", wasDeleted);
+        return response;
     }
 
     @RequestMapping(value = "/admin/authorities", method = RequestMethod.GET)
