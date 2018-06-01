b9fffdc329bd76805ee2b1addb6173aae34611fe
Mateusz Bednarski <echelon133@gmail.com>
2018-06-01 18:54:36 +0200

    Turned on the InMessage validation in the controller - now server rejects messages that break message length constraint

--- src/main/java/ml/echelon133/controller/RoomMessageController.java
+++ src/main/java/ml/echelon133/controller/RoomMessageController.java
@@ -7,9 +7,11 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -18,7 +20,7 @@ public class RoomMessageController {
     @MessageMapping("/{roomName}")
     @SendTo("/topic/{roomName}")
     public OutMessage roomMessageHandler(@DestinationVariable String roomName, OAuth2AuthenticationToken token,
-                                         InMessage inMessage) {
+                                         @Valid InMessage inMessage) {
         Map<String, Object> userAttributes = token.getPrincipal().getAttributes();
         String username = userAttributes.get("login").toString();
         String avatarUrl = userAttributes.get("avatar_url").toString();
@@ -30,5 +32,11 @@ public class RoomMessageController {
     public void conversionExceptionHandler(MessageConversionException ex) {
         // Do nothing - if this error happens it means that the user modified the client to send messages that cannot be
         // deserialized - probably message type was changed to a type that does not appear in MessageType enum
+    }
+
+    @MessageExceptionHandler
+    public void messageExceptionHandler(MethodArgumentNotValidException ex) {
+        // Do nothing - there is client-side validation of message length - if this error happens it means that the client
+        // was modified
     }
 }
