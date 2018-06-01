4403876dc3e9f8ca0916dbcbec6034283b8eaa2b
Mateusz Bednarski <echelon133@gmail.com>
2018-06-01 18:44:40 +0200

    Now server ignores malformed messages produced when the user modifies the client

--- src/main/java/ml/echelon133/controller/RoomMessageController.java
+++ src/main/java/ml/echelon133/controller/RoomMessageController.java
@@ -2,7 +2,9 @@ package ml.echelon133.controller;
 
 import ml.echelon133.model.message.InMessage;
 import ml.echelon133.model.message.OutMessage;
+import org.springframework.messaging.converter.MessageConversionException;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -22,5 +24,11 @@ public class RoomMessageController {
         String avatarUrl = userAttributes.get("avatar_url").toString();
         String profileUrl = userAttributes.get("html_url").toString();
         return new OutMessage(username, avatarUrl, profileUrl, inMessage.getMessageContent(), inMessage.getType());
+    }
+
+    @MessageExceptionHandler
+    public void conversionExceptionHandler(MessageConversionException ex) {
+        // Do nothing - if this error happens it means that the user modified the client to send messages that cannot be
+        // deserialized - probably message type was changed to a type that does not appear in MessageType enum
     }
 }
